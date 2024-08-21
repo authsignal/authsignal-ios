08a7325698fc74dd4ab9c532b23be6e2cdab888d
Chris Fisher <chris.i.fisher@gmail.com>
2024-08-21 14:49:49 +1200

    Add more fields to push challenge response (#28)

--- Sources/Authsignal/Models/PushChallenge.swift
+++ Sources/Authsignal/Models/PushChallenge.swift
@@ -1,5 +1,5 @@
-public struct PushChallengeResponse: Codable {
-  public let challengeId: String?
+public struct PushChallenge: Codable {
+  public let challengeId: String
   public let actionCode: String?
   public let idempotencyKey: String?
   public let deviceId: String?
