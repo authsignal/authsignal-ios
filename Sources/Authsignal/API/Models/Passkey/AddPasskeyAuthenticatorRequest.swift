c1a6bedac81db59905dd3447569b16c5dd267d04
Chris Fisher <chris.i.fisher@gmail.com>
2025-02-11 10:36:33 +1300

    Include device ID when adding passkey authenticator

--- Sources/Authsignal/API/Models/Passkey/AddPasskeyAuthenticatorRequest.swift
+++ Sources/Authsignal/API/Models/Passkey/AddPasskeyAuthenticatorRequest.swift
@@ -1,4 +1,5 @@
 public struct AddPasskeyAuthenticatorRequest: Codable {
   public let challengeId: String
   public let registrationCredential: PasskeyRegistrationCredential
+  public let deviceId: String?
 }
