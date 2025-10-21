6ca2a03a665ce06f8ca141a9abe258f7c1dc88c6
Chris Fisher <chris.i.fisher@gmail.com>
2025-10-22 09:58:54 +1300

    wip

--- Sources/Authsignal/Authsignal.swift
+++ Sources/Authsignal/Authsignal.swift
@@ -3,21 +3,21 @@ import Security
 import UIKit
 
 public class Authsignal {
-  public let app: AuthsignalInApp
+  public let inapp: AuthsignalInApp
   public let email: AuthsignalEmail
   public let passkey: AuthsignalPasskey
   public let push: AuthsignalPush
-  public let qrCode: AuthsignalQRCode
+  public let qr: AuthsignalQRCode
   public let sms: AuthsignalSMS
   public let totp: AuthsignalTOTP
   public let whatsapp: AuthsignalWhatsApp
 
   public init(tenantID: String, baseURL: String, deviceID: String? = nil) {
-    app = AuthsignalInApp(tenantID: tenantID, baseURL: baseURL)
+    inapp = AuthsignalInApp(tenantID: tenantID, baseURL: baseURL)
     email = AuthsignalEmail(tenantID: tenantID, baseURL: baseURL)
     passkey = AuthsignalPasskey(tenantID: tenantID, baseURL: baseURL, deviceID: deviceID)
     push = AuthsignalPush(tenantID: tenantID, baseURL: baseURL)
-    qrCode = AuthsignalQRCode(tenantID: tenantID, baseURL: baseURL)
+    qr = AuthsignalQRCode(tenantID: tenantID, baseURL: baseURL)
     sms = AuthsignalSMS(tenantID: tenantID, baseURL: baseURL)
     totp = AuthsignalTOTP(tenantID: tenantID, baseURL: baseURL)
     whatsapp = AuthsignalWhatsApp(tenantID: tenantID, baseURL: baseURL)
