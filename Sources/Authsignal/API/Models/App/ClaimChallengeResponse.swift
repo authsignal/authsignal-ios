8c6f87d6ef7f628a546a5d28a1d614949034dea9
Steven Clouston <stevenclouston@gmail.com>
2025-11-18 16:44:35 +1300

    AUT-3058 Return action code and idempotencyKey from claimChallenge

--- Sources/Authsignal/API/Models/App/ClaimChallengeResponse.swift
+++ Sources/Authsignal/API/Models/App/ClaimChallengeResponse.swift
@@ -2,4 +2,6 @@ public struct ClaimChallengeResponse: Codable {
   public let success: Bool
   public let ipAddress: String?
   public let userAgent: String?
+  public let actionCode: String?
+  public let idempotencyKey: String?
 } 
