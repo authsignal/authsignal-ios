6e1caa7b3a4661d48574261c527484d0bce15ac7
Steven Clouston <stevenclouston@gmail.com>
2026-03-17 16:15:31 +1300

    Fix missing default for appAttestation in AddCredentialRequest initialiser

--- Sources/Authsignal/API/Models/App/AddCredentialRequest.swift
+++ Sources/Authsignal/API/Models/App/AddCredentialRequest.swift
@@ -3,4 +3,16 @@ public struct AddCredentialRequest: Codable {
   public let deviceName: String
   public let devicePlatform: String
   public let appAttestation: AppAttestation?
+
+  public init(
+    publicKey: String,
+    deviceName: String,
+    devicePlatform: String,
+    appAttestation: AppAttestation? = nil
+  ) {
+    self.publicKey = publicKey
+    self.deviceName = deviceName
+    self.devicePlatform = devicePlatform
+    self.appAttestation = appAttestation
+  }
 }
